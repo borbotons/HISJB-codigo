cf9356712ce1637e2702fc033da623feb8fa6804
danielthewolf93 <danieltesorero@gmail.com>
2018-01-30 20:09:50 -0300

    Listado_usuarios_admin

--- controladores/listauser.php
+++ controladores/listauser.php
@@ -10,7 +10,37 @@ if (!isset($_SESSION['usuario'])) {
 }
 
 
-require '../sesion/listadouser.html';
+$resultado = '';
+
+$dni = 0;
+
+
+#Lista de usuarios 
+
+
+try {
+
+        $conexion = new PDO('mysql:host=localhost;dbname=hsjb','root','');
+
+
+    }catch(PDOException $e){
+
+       echo "Error:" .$e->getMessage();;
+    }
+
+   $statemente = $conexion->prepare('SELECT nombrecomp,division,dni,email FROM usuarios WHERE dni!= :dnis ORDER BY nombrecomp');
+
+    $statemente->execute(array(
+    ':dnis' => $dni
+    ));
+
+   $statemente->execute();
+
+   $resulta = $statemente->fetchALL();
+
+
+
+	require '../sesion/listadouser.html';
 
 
 
